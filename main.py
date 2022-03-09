32ec086918c6f9015f5da14b18a0137c547dd7be
Renan Santana <renan_ota@hotmail.com>
2022-03-09 19:54:48 -0300

    add hotkeys

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import serial
-import keyboard
+import pyautogui
 
 
 with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as serial_:
     serial_.reset_input_buffer()
 
     while True:
-        bytes_word = serial_.readline()
-        word = bytes_word.decode("utf-8")
-        word = word.replace("\n", "")
-        print(word)
-        print(word=="left")
+        bytes_signal = serial_.readline()
+        signal = bytes_signal.decode("utf-8")
+        signal = signal.replace("\n", "")
+
+        if signal == "left":
+            print("left")
+            pyautogui.hotkey("winleft", "left")
+        elif signal == "right":
+            print("right")
+            pyautogui.hotkey("winleft", "right")
+        elif signal == "up":
+            print("up")
+            pyautogui.hotkey("winleft", "up")
+        elif signal == "down":
+            print("down")
+            pyautogui.hotkey("winleft", "down")
